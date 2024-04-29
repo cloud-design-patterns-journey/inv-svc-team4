84392d170677e89e8db60a86ecf770316b32e50a
Kuruyia <github@kuruyia.net>
2024-04-29 12:24:26 +0200

    Added CRUD operations
    Signed-off-by: Kuruyia <github@kuruyia.net>

--- src/main/java/com/ibm/inventory_management/services/StockItemApi.java
+++ src/main/java/com/ibm/inventory_management/services/StockItemApi.java
@@ -6,4 +6,10 @@ import com.ibm.inventory_management.models.StockItem;
 
 public interface StockItemApi {
     List<StockItem> listStockItems();
+
+    void updateStockItem(String id, String name, String manufacturer, double price, int stock);
+
+    void addStockItem(String name, String manufacturer, double price, int stock);
+
+    void deleteStockItem(String id);
 }
